9d51b02dc8ae8021ea3a300513a891d720917e86
Cookie McCormick <muntananuchat@gmail.com>
2019-01-18 18:32:31 -0500

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,7 +1,8 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
   def change
     create_table :genres do |t|
-      t.string :name
+      t.string :name, null: false
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,9 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
     create_table :songs do |t|
-      t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
+      t.string :name, null: false
+      t.integer :artist_id, null: false
+      t.integer :genre_id, null: false
+      t.timestamps null: false
     end
   end
 end
