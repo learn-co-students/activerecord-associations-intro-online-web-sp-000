885a78ca4f3db26a7553f4d2ef9bfbe7b348572f
Anthony Tilelli <anthony@tilelli.me>
2020-01-15 03:41:24 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,24 @@
 
 ActiveRecord::Schema.define(version: 4) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
