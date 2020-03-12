7528dd1eb7dda98810994c334287e1c53fcb9769
imjoshellis <joshe523@gmail.com>
2020-03-11 22:35:55 -0500

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |t|
+    t.integer :artist_id
+    t.integer :genre_id
+    t.string :name
+    end
+  end
 end
