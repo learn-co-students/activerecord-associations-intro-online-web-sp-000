e50bd6449edf1850136e8515670c4d0538056581
Mark A Guza <guzamaguza@gmail.com>
2020-04-19 22:23:53 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+
+  def get_genre_name
+    self.genre.name
+  end
+
+  def get_artist_name
+    self.artist.name
+  end
 
 end
