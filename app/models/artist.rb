023b802a02167dc6b3c9ede28f1062519ca9a47c
Mark A Guza <guzamaguza@gmail.com>
2020-04-19 22:25:19 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+=begin
   def get_genre_of_first_song
     self.songs.first.genres  #return the genre of the Artist's first song
   end
@@ -9,6 +10,6 @@ class Artist < ActiveRecord::Base
   def get_song
     self.song.name
   end
-
+=end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,12 +2,4 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def get_genre_name
-    self.genre.name
-  end
-
-  def get_artist_name
-    self.artist.name
-  end
-
 end
