ccae49093f7ddaa777e5640097c2b942beb4bf35
cantstopcoding <jonathan_koss@yahoo.com>
2020-05-04 11:31:46 -0400

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateArtists < ActiveRecord::Migration[5.2]
     def change
       create_table :artists do |t|
         t.string :name
@@ -6,3 +6,4 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     end
   end
 
+  
