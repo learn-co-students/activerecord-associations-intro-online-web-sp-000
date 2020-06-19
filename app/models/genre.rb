5934ae9e69600a7c2d375d16ea3683de54fd5bfc
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-19 19:56:48 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,18 @@
 class Genre < ActiveRecord::Base
+has_many :songs
+has_many :artists, through: :songs
 end
+
+#a genre can have many songs
+
+
+
+#rake console to play along with code
+# pry> hello = Song.new(name: "Hello")
+# pry>hotline_bling = Song.new(name: "Hotline Bling")
+# pry>adele = Artist.new(name: "Adele")
+# pry>drake = Artist.new(name: "Drake")
+# pry>hello.artist = adele
+# pry>hello.artist => Adele
+# pry>hello.artist.name => "Adele"
+# pry>adele.songs.push(hello), adele.save, adele =>#<Artist:0x007fc75b8d9490 id: 1, name: "Adele">
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,12 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-end
+  def change
+      create_table :songs do |t|
+        t.string :name
+        t.integer :artist_id
+        t.integer :genre_id
+      end
+    end
+  end
+
+
+#  A song will belong to an artist and belong to a genre. 
