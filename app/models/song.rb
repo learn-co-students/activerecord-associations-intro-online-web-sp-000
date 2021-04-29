2aa4f1c33f3c363bd46e767a1e31616772de5769
Hannah M <hannahemalm616@gmail.com>
2021-04-29 18:47:03 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
-
+belongs_to :artist #tell the Song class that it will produce objects that can belong to an artist
+belongs_to :genre #songs belong to a genre 
 end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
+  def change #create an artists table that only has a name
+    create_table :artists do |t| 
+      t.string :name 
+    end 
+  end
 end
